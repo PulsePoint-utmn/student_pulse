b77b525b4601a3b286fd3eec31024777d7d46c2f
Juan724r <hasan.abushev@gmail.com>
2023-10-21 21:34:21 +0500

    add header structure

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,18 +4,8 @@ import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <div className='mont-reg'>
-        adasdasdasd
-      </div>
-      <div className='mont-bold'>
-        adlaskdasld
-      </div>
-      <div>
-        sadasldnaslds
-      </div>
+    
       <Header/>
-    </div>
   );
 }
 
