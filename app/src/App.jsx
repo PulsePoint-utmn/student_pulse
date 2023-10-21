9395e2a68fb1076d7cce3b20cc3701f4311dca50
Juan724r <hasan.abushev@gmail.com>
2023-10-21 21:20:43 +0500

    add font, main blocks

---
+++ app/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='mont-reg'>
+        adasdasdasd
+      </div>
+      <div className='mont-bold'>
+        adlaskdasld
+      </div>
+      <div>
+        sadasldnaslds
+      </div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
